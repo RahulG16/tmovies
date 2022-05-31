fb0e726e9539355aae53dc3c59a74b419959b150
Rahul G <rahulganga023@gmail.com>
2022-05-31 17:55:29 +0530

    Route Fix

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import FilmInfoProvider from "./Store/FilmInfoProvider";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-      <BrowserRouter>
+      <HashRouter>
       <FilmInfoProvider>
         <App />
       </FilmInfoProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
